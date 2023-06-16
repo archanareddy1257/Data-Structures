ed446f5c99a0aeb8498c3b73c5b18066e835f9df
archanareddy1257 <106471758+archanareddy1257@users.noreply.github.com>
2023-06-16 18:13:31 +0530

    Linear Search.py

--- Searching/Linear Search.py
+++ Searching/Linear Search.py
@@ -1,3 +1,4 @@
+#arrays
 def fun(arr,n,t):
     for i in range(n):
         if arr[i]==t:
@@ -5,7 +6,48 @@ def fun(arr,n,t):
     return -1
         
 
-n=6
-arr=[1,4,2,5,9,8]
-t=5
+n=int(input("enter the size of array:"))
+print("Enter array elements:")
+arr=[]
+for i in range(1,n+1):
+    ele=int(input())
+    arr.append(ele)
+t=int(input("enter the target variable:"))
 fun(arr,n,t)
+#Linked list:
+class LinkedList:
+    def __init__(self):
+        self.head=None
+    def print(self):
+        if self.head is None:
+            print("empty")
+        else:
+            n=self.head
+            while n is not None:
+                print(n.data)
+                n=n.ref
+    def add_end(self,data):
+        newnode=Node(data)
+        if self.head is None:
+            self.head=newnode
+        else:
+            n=self.head
+            while n.ref is not None:
+                n=n.ref
+            n.ref=newnode
+    def search(self,x):
+        if self.head is None:
+            print("empty")
+        else:
+            n=self.head
+            while n is not None:
+                if x==n.data:
+                    print("yes")
+                n=n.ref
+        
+
+LL1=LinkedList()
+LL1.add_end(10)
+LL1.add_end(20)
+LL1.add_end(30)
+LL1.search(45)
