2b07390c97ee08eaeec25171d3e528b121e903dd
archanareddy1257 <106471758+archanareddy1257@users.noreply.github.com>
2023-07-17 23:40:53 +0530

    queue.py

--- python_ds/queue.py
+++ python_ds/queue.py
@@ -40,5 +40,42 @@ x.append(1)
 x.popleft()
 print(x)
 
-
+# Using 2 stacks
+def Push(x,stack1,stack2):
+    while len(stack1)!=0:
+        stack2.append(stack1[-1])
+        stack1.pop()
+    stack1.append(x)
+    while len(stack2)!=0:
+        stack1.append(stack2[-1])
+        stack2.pop()
+#Function to pop an element from queue by using 2 stacks.
+def Pop(stack1,stack2):
+    #code here
+    if len(stack1)==0:
+        return -1
+    x=stack1[-1]
+    stack1.pop()
+    return x
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        qry=int(input())
+        qtyp_qry=list(map(int, input().strip().split()))
+        
+        i=0
+        stack1=[]
+        stack2=[]
+        while i <len(qtyp_qry):
+            #print(i)
+            if qtyp_qry[i]==1:
+                Push(qtyp_qry[i+1],stack1,stack2)
+                #print(i)
+                i+=2
+            else:
+                print(Pop(stack1,stack2),end=' ')
+                i+=1
+                
+        print()
+# } Driver Code Ends
     
